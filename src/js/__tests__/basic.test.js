f990f1b8bd71e3e281ec7fc6c49bc9d28b4ff779
lanoriya <Mr_Buzik@mail.ru>
2024-04-04 22:16:27 +0500

    Add files

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,5 @@
+describe("card check container", () => {
+  test("check 1", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/js/__tests__/e3e.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+describe('check container', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:8080');
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it('check click', async () => {
+    await page.waitForSelector('.btn-pop');
+    await page.click('.btn-pop');
+
+    await page.waitForSelector('.pop-content');
+
+    const popover = await page.$('.pop-content');
+    expect(popover).toBeTruthy();
+  });
+
+});
